31ba4d44117ea2cac90520bc2556323dbd871237
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2023-12-17 15:48:19 +0700

    feat(go): re structure project directory

---
+++ print-message/go.mod
@@ -0,0 +1,3 @@
+module print-message
+
+go 1.21.0
---
+++ print-message/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Printf("Hi my name is %v, I %v years old \n", "Irfan", 16)
+}
